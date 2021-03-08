e08f251cc9dfb426bb27b8b5d85a30b0e8228181
vivek kumar <vivek199575@gmail.com>
2021-03-08 16:02:53 +0530

    fist

---
+++ routes/pages.js
@@ -0,0 +1,22 @@
+const express=require('express');
+
+const router =express.Router();
+
+router.get('/',(req,res)=>{
+    res.render('index');
+})
+
+router.get('/register',(req,res)=> {
+    res.render('register');
+})
+
+router.get('/login',(req,res)=> {
+    res.render('login');
+})
+
+router.get('/about',(req,res)=> {
+    res.render('about');
+})
+
+
+module.exports=router;
